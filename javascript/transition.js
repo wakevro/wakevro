3dbc1af9fa987dd515d236e39feb5f3b5600e427
wakevro <wakevro@gmail.com>
2022-07-27 16:08:43 -0700

    Deleted unused files.

--- javascript/transition.js
+++
@@ -1,9 +0,0 @@
-const link = document.querySelector(".link");
-const transition = document.querySelector(".transition");
-link.addEventListener("click", (e) => {
-  e.preventDefault();
-  transition.classList.add("slide");
-  setTimeout(() => {
-    window.location = link.href;
-  }, 900);
-});
